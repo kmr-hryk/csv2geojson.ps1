2c19090e6e30da4d41b75fe1e422a7f4b558b831
Hiroyuki Kimura <z_kimura@k-side.net>
2015-06-19 14:50:40 +0900

    変換機能を概ね実装

---
+++ csv2geojson.ps1
@@ -0,0 +1,61 @@
+#必須引数のチェック
+Param (
+   [Parameter(mandatory)]$X,
+   [Parameter(mandatory)]$Y,
+   [Parameter(mandatory)]$code,
+   [Parameter(mandatory)]$csv
+)
+
+#CSVファイルのフルパスを取得
+$Filename = $(Get-ChildItem $csv).FullName
+
+#$csv_data = Import-Csv $Filename
+
+#$json_data = ConvertTo-Json $csv_data
+
+#Write-Output $json_data.length
+
+#要素の名前を取得
+$fieldname = $(Get-Content $Filename | Select-Object -First 1) -split ","
+$Column_count = $fieldname.length
+
+#データを取得
+$data_body = $(Get-Content $Filename | Select-Object -Skip 1)
+
+#EPSGコードの指定
+$code = $code - 1
+$crs_collection = @( "2443", "2444", "2445", "2446", "2447", "2448", "2449", "2450", "2451", "2452", "2453", "2454", "2455", "2456", "2457", "2458", "2459", "2460", "2461")
+
+#GeoJSONの出力
+Write-Output $('{')
+Write-Output $('"type": "FeatureCollection",')
+#EPSGコード部分
+Write-Output $('"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::' + $($crs_collection[$code]) + '" } },')
+Write-Output $('"features": [')
+
+#feature部分
+$LineNum = 0
+$data_body | ForEach-Object {
+   $element = ""
+   $body = $_ -split ","
+   for ($i=0;$i -le $Column_count; $i++) {
+      $name = $fieldname[$i]
+      $value = $body[$i]
+      if ( $name -ne $null) {
+         $element = $element + ' "' + $name + '": "' + $value + '",'
+      }
+   }
+   $coodinates = '[ ' + $body[$Y -1] + ', ' + $body[$X -1] + ' ]'
+   $element = $element.Trim(",") + " "
+   $feature = $('{ "type": "Feature", "properties": {' + $element + '}, "geometry": { "type": "Point", "coordinates": ' + $coodinates + ' } }')
+   if ( $LineNum -ne ($data_body.length -1)) {
+      $feature = $feature + ","
+   }
+   Write-Output $feature
+
+   $LineNum++
+}
+
+#おしまい
+Write-Output ']'
+Write-Output '}'
